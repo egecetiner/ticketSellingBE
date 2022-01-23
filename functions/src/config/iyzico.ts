c26596bed5a0413c7b1d59220f68105c4db1743b
Truwabeygiri <egeecetiner@gmail.com>
2022-01-23 15:47:28 +0300

    first commit

--- functions/src/config/iyzico.ts
+++
@@ -1,9 +0,0 @@
-import * as Iyzipay from "iyzipay";
-
-var iyzipay = new Iyzipay({
-    apiKey: 'sandbox-sLUwoYi2g3m2kuqShekLopqQqiMIe2Qa',
-    secretKey: "sandbox-nxaKfFFYL2SV47CtjfV4lcFrZlIHE88E",
-    uri: 'https://sandbox-api.iyzipay.com'
-});
-
-export { iyzipay };
